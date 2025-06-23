ffa4b29d1398e33f690e61aa950c0924589c4393
Forrest Williams <ffwilliams2@alaska.edu>
2025-06-23 07:23:16 -0500

    array 1, 2

--- chapter_01/p01_is_unique.py
+++ chapter_01/p01_is_unique.py
@@ -2,6 +2,8 @@ import time
 import unittest
 from collections import defaultdict
 
+from s01_is_unique import is_unique_forrest
+
 
 def is_unique_chars_algorithmic(string):
     # Assuming character set is ASCII (128 characters)
@@ -80,13 +82,13 @@ def is_unique_chars_sort(string: str) -> bool:
 
 class Test(unittest.TestCase):
     test_cases = [
-        ("abcd", True),
-        ("s4fad", True),
-        ("", True),
-        ("23ds2", False),
-        ("hb 627jh=j ()", False),
-        ("".join([chr(val) for val in range(128)]), True),  # unique 128 chars
-        ("".join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
+        ('abcd', True),
+        ('s4fad', True),
+        ('', True),
+        ('23ds2', False),
+        ('hb 627jh=j ()', False),
+        (''.join([chr(val) for val in range(128)]), True),  # unique 128 chars
+        (''.join([chr(val // 2) for val in range(129)]), False),  # non-unique 129 chars
     ]
     test_functions = [
         is_unique_chars_pythonic,
@@ -96,6 +98,7 @@ class Test(unittest.TestCase):
         is_unique_chars_using_set,
         is_unique_chars_sorting,
         is_unique_chars_sort,
+        is_unique_forrest,
     ]
 
     def test_is_unique_chars(self):
@@ -106,17 +109,13 @@ class Test(unittest.TestCase):
             for text, expected in self.test_cases:
                 for is_unique_chars in self.test_functions:
                     start = time.perf_counter()
-                    assert (
-                        is_unique_chars(text) == expected
-                    ), f"{is_unique_chars.__name__} failed for value: {text}"
-                    function_runtimes[is_unique_chars.__name__] += (
-                        time.perf_counter() - start
-                    ) * 1000
+                    assert is_unique_chars(text) == expected, f'{is_unique_chars.__name__} failed for value: {text}'
+                    function_runtimes[is_unique_chars.__name__] += (time.perf_counter() - start) * 1000
 
-        print(f"\n{num_runs} runs")
+        print(f'\n{num_runs} runs')
         for function_name, runtime in function_runtimes.items():
-            print(f"{function_name}: {runtime:.1f}ms")
+            print(f'{function_name}: {runtime:.1f}ms')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- chapter_01/p02_check_permutation.py
+++ chapter_01/p02_check_permutation.py
@@ -2,6 +2,8 @@
 import unittest
 from collections import Counter
 
+from s02_check_permutation import check_permutation_forrest
+
 
 def check_permutation_by_sort(s1, s2):
     if len(s1) != len(s2):
@@ -38,23 +40,24 @@ def check_permutation_pythonic(str1, str2):
 class Test(unittest.TestCase):
     # str1, str2, is_permutation
     test_cases = (
-        ("dog", "god", True),
-        ("abcd", "bacd", True),
-        ("3563476", "7334566", True),
-        ("wef34f", "wffe34", True),
-        ("dogx", "godz", False),
-        ("abcd", "d2cba", False),
-        ("2354", "1234", False),
-        ("dcw4f", "dcw5f", False),
-        ("DOG", "dog", False),
-        ("dog ", "dog", False),
-        ("aaab", "bbba", False),
+        ('dog', 'god', True),
+        ('abcd', 'bacd', True),
+        ('3563476', '7334566', True),
+        ('wef34f', 'wffe34', True),
+        ('dogx', 'godz', False),
+        ('abcd', 'd2cba', False),
+        ('2354', '1234', False),
+        ('dcw4f', 'dcw5f', False),
+        ('DOG', 'dog', False),
+        ('dog ', 'dog', False),
+        ('aaab', 'bbba', False),
     )
 
     testable_functions = [
         check_permutation_by_sort,
         check_permutation_by_count,
         check_permutation_pythonic,
+        check_permutation_forrest,
     ]
 
     def test_cp(self):
@@ -64,5 +67,5 @@ class Test(unittest.TestCase):
                 assert check_permutation(str1, str2) == expected
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
